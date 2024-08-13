c56f8bffa5b07acbfe68e14cfb8005756b3ad16b
0x-alx <alx.charbo@gmail.com>
2024-08-13 18:08:13 +0200

    Init

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const cron = require("node-cron");
+
+const futureDate = new Date("2024-12-31T23:59:59");
+
+const generateCountdown = () => {
+	const DateNow = new Date();
+
+	const timeDifference = futureDate - DateNow;
+
+	const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+	const hours = Math.floor(
+		(timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+	);
+	const minutes = Math.floor(
+		(timeDifference % (1000 * 60 * 60)) / (1000 * 60)
+	);
+	const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+	console.log(
+		`Compte à rebours: ${days} jours, ${hours} heures, ${minutes} minutes, ${seconds} secondes`
+	);
+};
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+	res.send("Target reports sent successfully");
+	generateCountdown();
+});
+
+app.listen(3000, () => {
+	console.log("Server is running on port 3000");
+});
+
+// cron.schedule("0/1 * * * * *", generateCountdown);
